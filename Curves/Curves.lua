a58ef66a34d64c4e2c9e931eac4999d9a4fd4364
ChensenCHX <2087826155@qq.com>
2022-08-22 13:18:29 +0800

    曲线库大改 支持n项参方插值与自定义插值函数！ 你甚至可以根据x，y坐标插颜色（草）

--- Curves/Curves.lua
+++ Curves/Curves.lua
@@ -7,11 +7,15 @@ local CurveTable = {}
 
 ---一般通过检查参数是否有问题的函数
 ---防范小天才,从我做起(欸嘿)
-local function CheckCurve(name, ctrlinfo, mode)
+local function CheckCurve(name, curinfo, ctrlinfo)
     if CurveTable[name] then
         Print('[Curve Warn] ', name, ' has been used, old data will be delete!')
     else
-        CurveTable[name] = {oriposx = {}, oriposy = {}}
+        CurveTable[name] = {}
+        if #curinfo < 1 then error('Parametric equation must have at least 1 equation!', 2) end
+        for k, _ in pairs(curinfo) do
+            CurveTable[name][k] = {}
+        end
     end
     if ctrlinfo.sZoneLow >= ctrlinfo.sZoneHigh then
         error('Invalid sampling zone!', 2)
@@ -25,42 +29,44 @@ end
 ---@param name string 该曲线名称
 ---@param curinfo table 包含曲线信息的表
 ---@param ctrlinfo table 包含采样参数的表
----curinfo表形如{x = function(t) end, y = function(t) end}, 函数的返回值应当为传入的参数的对应值  
+---curinfo表形如{x = function(t) end, y = function(t) end}, 函数的返回值应当为传入的参数的对应值
+---注意,上述x,y等仅作为示例,实际上参方的输出可以任意多维  
 ---ctrlinfo表形如{sLength = 采样参数变化步长, sZoneLow = 采样区域下界, sZoneHigh = 采样区域上界}  
 ---注意,如果采样区域无法被步长整除,多余的部分将被舍弃!  
 function lib.SamplingCurve(name, curinfo, ctrlinfo)
-    CheckCurve(name, ctrlinfo)
+    CheckCurve(name, curinfo, ctrlinfo)
     local sZoneLength = ctrlinfo.sZoneHigh - ctrlinfo.sZoneLow
     sZoneLength = sZoneLength - sZoneLength%ctrlinfo.sLength
     for _ = ctrlinfo.sZoneLow, sZoneLength do
-        CurveTable[name].oriposx[_] = curinfo.x(_)
-        CurveTable[name].oriposy[_] = curinfo.y(_)
+        for k, v in pairs(curinfo) do
+            CurveTable[name][k] = curinfo[k](_)
+        end
     end
 end
 
----对一个已采样曲线取等距点
----如果采样精度很糟糕,取点也会很糟糕!!!
----只是一般通过简单逼近实现
+---对一个已采样曲线进行插值  
+---如果采样精度很糟糕,取点也会很糟糕!!!  
+---只是一般通过简单逼近实现  
+---由于一些原因(我懒) 各种参数不会被以任何形式检查!!(当前版本)  
+---所以给我看好了再传参......!!!  
+---小贴士:你传入的函数可以访问到result哦(通过访问传入的result)(仅访问,并不能修改值!!)
 ---@param name string 曲线名称
----@param length number 目标长度
----@return result table 用于储存结果的表  result.x, result.y表分别存储x坐标y坐标(以数组形式排序)
-function lib.Interpolation(name, length)
+---@param Infolist table 插值所需信息,按{var,var,...,var}排序,值均为字符串
+---@param Ifunc function 插值函数,参数列表按(var,var,...var,result)排序,顺序应同Infolist顺序,返回值应为目标值或nil
+---@return result table 用于储存插值结果的表(以数组形式排序),若Ifunc返回值为nil则等待下一返回值
+function lib.Interpolation(name, Infolist, Ifunc)
     local result = {}
-    result.x = {CurveTable[name].oriposx[1]}
-    result.y = {CurveTable[name].oriposy[1]}
-    if not CurveTable[name] then
-        error('Invalid Curve name, \'' .. name .. '\' Curve doens`t exist!')
-    end
-    if length <= 0 then
-        error('Invalid interpolation length!')
-    end
-    local dist = 0
-    local tmpdist
-    for _ = 1, #CurveTable[name].oriposx do
-        tmpdist = Dist(result.x[#result.x], result.y[#result.y], CurveTable[name].oriposx[_], CurveTable[name].oriposy[_])
-        if abs(dist + tmpdist - length) > abs(dist - length) then
-            table.insert(result.x, CurveTable[name].oriposx[_])
-            table.insert(result.y, CurveTable[name].oriposy[_])
+    local tmplist = {}
+    local length = #Infolist
+    local tmp
+    for _ = 1, #CurveTable[name][Infolist[1]] do
+        for i = 1, length do
+            tmplist[i] = CurveTable[name][Infolist[i]][_]
+        end
+        table.insert(tmplist, result)
+        tmp = Ifunc(unpack(tmplist))
+        if tmp then
+            table.insert(result, tmp)
         end
     end
     return result
@@ -76,8 +82,20 @@ function lib.GetAllCurve()
     return Curves
 end
 
+---返回name对应曲线的所有采样项名称
+---@param name string 曲线名称
+---@return table 用于承装名称的表
+function lib.GetCurveParaName(name)
+    local result = {}
+    for k, v in pairs(CurveTable[name]) do
+        table.insert(result, k)
+    end
+    return result
+end
+
 ---删除一个已采样曲线的数据
 ---@param name string 曲线名称
 function lib.DeleteCurve(name)
     CurveTable[name] = nil
 end
+
