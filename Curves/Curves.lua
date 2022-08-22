6486d81f9e80586403c1800485505bfe734788eb
ChensenCHX <2087826155@qq.com>
2022-08-22 13:53:23 +0800

    曲线库提供一个自带的插值函数 也可以作为参考，用来自己造插值函数（）

--- Curves/Curves.lua
+++ Curves/Curves.lua
@@ -52,8 +52,8 @@ end
 ---小贴士:你传入的函数可以访问到result哦(通过访问传入的result)(仅访问,并不能修改值!!)
 ---@param name string 曲线名称
 ---@param Infolist table 插值所需信息,按{var,var,...,var}排序,值均为字符串
----@param Ifunc function 插值函数,参数列表按(var,var,...var,result)排序,顺序应同Infolist顺序,返回值应为目标值或nil
----@return result table 用于储存插值结果的表(以数组形式排序),若Ifunc返回值为nil则等待下一返回值
+---@param Ifunc function 插值函数,参数列表按(var,var,...var,result)排序,顺序应同Infolist顺序,返回值应为目标值或false/nil
+---@return result table 用于储存插值结果的表(以数组形式排序),若Ifunc返回值为false/nil则等待下一返回值
 function lib.Interpolation(name, Infolist, Ifunc)
     local result = {}
     local tmplist = {}
@@ -99,3 +99,29 @@ function lib.DeleteCurve(name)
     CurveTable[name] = nil
 end
 
+---提供一些写好的插值函数:
+---等距取点  
+---最终插值返回的表形如{{x1, y1}, {x2, y2}, ...}  
+---通过闭包形式保存一些信息,实际使用应如下操作   
+---xxx = lib.Equidistant(yyy),向插值函数传递xxx  
+function lib.Equidistant(length)
+    local tmp = {dist = 0}
+    return function(x, y, result)
+        if #result == 0 then
+            tmp.x, tmp.y = x, y
+            return {x, y}
+        end
+        local dist = Dist(tmp.x, tmp.y, x, y)
+        if abs(tmp.dist - length) > abs(tmp.dist + dist - length) then
+            tmp.x, tmp.y = x, y
+            tmp.dist = tmp.dist + dist
+            return false
+        else
+            tmp.x, tmp.y = x, y
+            tmp.dist = 0
+            return {x, y}
+        end
+    end
+end
+
+    
