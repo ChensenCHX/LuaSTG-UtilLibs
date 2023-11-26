e2771c08b0eb01f78508c756b1af7ae67635715f
ChensenCHX <2087826155@qq.com>
2023-11-26 13:11:59 +0800

    糊依托构屎 派生（复制）功能做了 一个示例性质的Image类型 等会做一下小实验就好

--- ResObject/ResObject.lua
+++ ResObject/ResObject.lua
@@ -50,7 +50,8 @@ local function pClass(base)
     return class
 end
 
--------
+-------一些枚举量和常量-------
+
 local typeEnum = {
     Texture = 1,
     Image = 2,
@@ -63,10 +64,18 @@ local typeEnum = {
     FX = 9
 }
 
+local nullName = "null"
+local deriveTag = "__derive"
+
+-------异步处理队列与异步处理对象-------
+
 local asyncQueue = {size = 32768, head = 0, tail = 0}
 function asyncQueue.push(t)
     asyncQueue[asyncQueue.tail] = t
     asyncQueue.tail = (asyncQueue.tail + 1) % asyncQueue.size
+    if asyncQueue.tail == asyncQueue.head then
+        error("AsyncQueue is full. Try using higher process speed or bigger queue.")
+    end
 end
 function asyncQueue.pop()
     local data = asyncQueue[asyncQueue.head]
@@ -74,27 +83,83 @@ function asyncQueue.pop()
     asyncQueue.head = (asyncQueue.head + 1) % asyncQueue.size
     return data
 end
+function asyncQueue.empty()
+    return asyncQueue.head == asyncQueue.tail
+end
 
-local nullName = "null"
+lib.asyncProcessor = pClass()
 
+---创建一个异步处理器对象
+---@param speed number 处理速度 每帧处理多少个Load请求
+---@param patcher lstg.GameObject 挂载对象 可以直接New(objectName)
+---@param terminate boolean 处理队列空后是否终止 ture时会在异步处理队列空后销毁自身和所挂载的对象
+---@param wait number terminate为false时若队列为空等待多久再进行加载检测
+function lib.asyncProcessor:init(speed, patcher, terminate, wait)
+    self.counter = 0
+    if terminate then
+        task.New(patcher, function()
+            while true do
+                while self.counter < speed do
+                    if asyncQueue.empty() then break end
+                    
+                    local t = asyncQueue.pop()
+                    if t[1]:find("Load") then
+                        self.counter = self.counter + 1
+                    end
+                    _G[t[1]](unpack(t, 2))
+                end
+
+                self.counter = 0
+                task.Wait()
+                if asyncQueue.empty() then break end
+            end
+            Del(patcher)
+        end)
+    else
+        task.New(patcher, function()
+            while true do
+                while self.counter < speed do
+                    if asyncQueue.empty() then break end
+
+                    local t = asyncQueue.pop()
+                    if t[1]:find("Load") then
+                        self.counter = self.counter + 1
+                    end
+                    _G[t[1]](unpack(t, 2))
+                end
+
+                self.counter = 0
+                task.Wait()
+                if asyncQueue.empty() then task.Wait(wait) end
+            end
+        end)
+    end
+end
+
+-------基类以及基本功能-------
+
+---@class ResObj
 lib.ResObj = pClass()
 function lib.ResObj:init()
     self.name = nullName
     self.type = 0
+    self.rclass = lib.ResObj
     self.derive = {}
 end
 
 ---派生一个资源对象 修改它不会影响源对象
-function lib.ResObj:Derive()
-    --TODO
+---@param async boolean
+---@return ResObj _ 派生资源对象
+function lib.ResObj:Derive(async)
+    return self.rclass(async, self.loadInfo[1], self.loadInfo[2] .. deriveTag, unpack(self.loadInfo, 3))
 end
 
 ---资源对象加载
+---@param async boolean 是否异步加载 安全起见,异步加载的资源全部会被导出到global池
 ---@param type 'Texture'|'Image'|'Animation'|'Music'|'Sound'|'PS'|'Font'|'TTF'|'FX' 资源类型
 ---@param name string 资源实名称
----@param async boolean 是否异步加载 安全起见,异步加载的资源全部会被导出到global池
 ---@param ... any 所需的额外的加载信息
-function lib.ResObj:Load(type, name, async, ...)
+function lib.ResObj:Load(async, type, name, ...)
     if self.type == 0 then
         self.type = assert(typeEnum[type], 'Invalid resource type \'', type, '\'')
     else
@@ -111,33 +176,56 @@ function lib.ResObj:Load(type, name, async, ...)
     else
         _G['Load' .. type](name, ...)
     end
+
+    self.loadInfo = {type, name, ...}
     return self
 end
 
 ---资源对象重加载  
 ---警告 此行为会切断派生关系 如果有派生对象(比如说 出于性能目的缓存对象)无法通过释放该对象来自动释放
+---@param async boolean 是否异步加载 安全起见,异步加载的资源全部会被导出到global池
 ---@param type 'Texture'|'Image'|'Animation'|'Music'|'Sound'|'PS'|'Font'|'TTF'|'FX' 资源类型
 ---@param name string 资源实名称
----@param async boolean 是否异步加载 安全起见,异步加载的资源全部会被导出到global池
 ---@param ... any 所需的额外的加载信息
-function lib.ResObj:ReLoad(type, name, async, ...)
+function lib.ResObj:ReLoad(async, type, name, ...)
     self.derive = {}
     if async then
-        asyncQueue.push({'lstg.RemoveResource' .. type, 'global', self.type, self.name})
+        asyncQueue.push({'RemoveResource', 'global', self.type, self.name})
         self.type = assert(typeEnum[type], 'Invalid resource type \'', type, '\'')
         assert(not lstg.CheckRes(self.type, name), string.format('Resource %s with type %s is already exist!', name, type))
         asyncQueue.push({'Load' .. type, name, ...})
     else
-        lstg.RemoveResource('global', self.type, self.name)
+        lstg.RemoveResource(lstg.CheckRes(self.type, self.name), self.type, self.name)
         self.type = assert(typeEnum[type], 'Invalid resource type \'', type, '\'')
         assert(not lstg.CheckRes(self.type, name), string.format('Resource %s with type %s is already exist!', name, type))
         _G['Load' .. type](name, ...)
     end
+
     self.name = name
+    self.loadInfo = {type, name, ...}
     return self
 end
 
+---释放掉已经加载的资源
+---@param async boolean 是否异步释放 注意异步操作只会操作global池的资源该名称同类型资源
+---@param recursion boolean 是否递归释放 若是则其派生类也被释放 否则会解除派生关系
+function lib.ResObj:Delete(async, recursion)
+    if recursion then
+        for _, res in ipairs(self.derive) do
+            res:Delete(async)
+        end
+    else
+        self.derive = {}
+    end
 
+    if async then
+        asyncQueue.push({'RemoveResource' .. type, 'global', self.type, self.name})
+    else
+        lstg.RemoveResource(lstg.CheckRes(self.type, self.name), self.type, self.name)
+    end
+    self.name = nullName
+    self.type = 0
+end
 
 ---使用资源对象 该函数直接返回资源名称
 function lib.ResObj:Use()
@@ -156,14 +244,26 @@ function lib.ResObj:AsyncUse(target, targetVarName)
     end)
 end
 
+-------一些便用的包装-------
+
 lib.Image = pClass(lib.ResObj)
+
+---加载图片(从纹理)
+---@param async boolean 是否异步加载
+---@param name string 图片资源名称
+---@param ... any LoadImage函数的其余参数(除name) 
+function lib.Image:Load(async, name, ...)
+    lib.ResObj.Load(self, async, 'Image', name, ...)
+    self.rclass = lib.Image
+end
+lib.Image.init = lib.Image.Load
 
 ---设置图片渲染缩放, 默认为1
 ---@param async any
 ---@param scale any
 function lib.Image:SetScale(async, scale)
     if async then
-        asyncQueue.push({'lstg.SetImageScale', self.name, scale})
+        asyncQueue.push({'SetImageScale', self.name, scale})
     else
         lstg.SetImageScale(self.name, scale)
     end
@@ -175,9 +275,18 @@ end
 ---@param ... lstg.Color 顶点颜色
 function lib.Image:SetState(async, blendmode, ...)
     if async then
-        asyncQueue.push({'lstg.SetImageState', self.name, blendmode, ...})
+        asyncQueue.push({'SetImageState', self.name, blendmode, ...})
     else
         lstg.SetImageState(self.name, blendmode, ...)
+    end
+    return self
+end
+
+function lib.Image:SetCenter(async, x, y)
+    if async then
+        asyncQueue.push({'SetImageCenter', self.name, x, y})
+    else
+        lstg.SetImageState(self.name, x, y)
     end
     return self
 end
